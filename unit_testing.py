e08c8f9cc126553d684615ea16efeb827ece0b8d
pranavbrkr <pranavbrkr@gmail.com>
2020-11-05 16:51:09 +0530

    More tests added

--- unit_testing.py
+++ unit_testing.py
@@ -12,10 +12,58 @@ class CarTest(unittest.TestCase):
         urban = Urban("urban", 900)
         self.assertEqual(self.car.mapping(urban)["totalDistanceTravelled"], 1000)
 
+    def test_urban1(self):
+        urban = Urban("urban", 150)
+        self.assertEqual(self.car.mapping(urban)["totalDistanceTravelled"], 200)
+
+    def test_urban2(self):
+        urban = Urban("urban", 4000)
+        self.assertEqual(self.car.mapping(urban)["totalDistanceTravelled"], 4300)
+
+    def test_urban3(self):
+        urban = Urban("urban", 40)
+        self.assertEqual(self.car.mapping(urban)["totalDistanceTravelled"], 80)
+
+    def test_urban4(self):
+        urban = Urban("urban", 650)
+        self.assertEqual(self.car.mapping(urban)["totalDistanceTravelled"], 730)
+
     def test_rural(self):
         rural = Rural("rural", 900)
-        self.assertEqual(self.car.mapping(rural)["totalDistanceTravelled"], 1050)
+        self.assertEqual(self.car.mapping(rural)["totalDistanceTravelled"], 1040)
 
+    def test_rural1(self):
+        rural = Rural("rural", 150)
+        self.assertEqual(self.car.mapping(rural)["totalDistanceTravelled"], 260)
+
+    def test_rural2(self):
+        rural = Rural("rural", 4000)
+        self.assertEqual(self.car.mapping(rural)["totalDistanceTravelled"], 4300)
+
+    def test_rural3(self):
+        rural = Rural("rural", 40)
+        self.assertEqual(self.car.mapping(rural)["totalDistanceTravelled"], 140)
+
+    def test_rural4(self):
+        rural = Rural("rural", 650)
+        self.assertEqual(self.car.mapping(rural)["totalDistanceTravelled"], 780)
+
+    
+    def test_refuel_urban1(self):
+        urban = Urban("urban", 1200)
+        self.assertEqual(self.car.mapping(urban)["totalRefuels"], 8)
+
+    def test_refuel_urban2(self):
+        urban = Urban("urban", 5321)
+        self.assertEqual(self.car.mapping(urban)["totalRefuels"], 35)
+
+    def test_refuel_rural1(self):
+        rural = Rural("rural", 1200)
+        self.assertEqual(self.car.mapping(rural)["totalRefuels"], 6)
+
+    def test_refuel_rural2(self):
+        rural = Rural("rural", 5321)
+        self.assertEqual(self.car.mapping(rural)["totalRefuels"], 26)
 
     def test_largeinput_urban(self):
         urban = Urban("urban", 463808250)
@@ -26,13 +74,6 @@ class CarTest(unittest.TestCase):
         rural = Rural("rural", 900539000)
         self.assertIsNotNone(self.car.mapping(rural)["totalDistanceTravelled"])
     
-    # def test_negative_urban(self):
-    #     rural = Rural("rural", -10)
-    #     self.assertEqual(self.car.mapping(rural)["totalDistanceTravelled"], 0)
-
-    # def test_negative_rural(self):
-    #     urban = Urban("urban", -50)
-    #     self.assertEqual(self.car.mapping(urban)["totalDistanceTravelled"], 0)
 
 if __name__ == '__main__':
     unittest.main()
